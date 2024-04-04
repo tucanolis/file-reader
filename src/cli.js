ac25f745ed549ac0bf571bc6d65cb459f51b3779
tucanolis <arthur.sinmc@gmail.com>
2024-04-04 10:36:53 -0300

    chore

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,20 @@
 import pegarArquivo from "./index.js";
 import chalk from "chalk";
-
+import fs from "fs";
 const caminho = process.argv;
 
-async function processarTexto (caminho) {
-    const resultado = await pegarArquivo(caminho[2]);
-    console.log(chalk.yellow("Lista de links"), resultado);
+async function processarTexto(argumentos) {
+  const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegarArquivo(argumentos[2]);
+        console.log(chalk.yellow("Lista de links"), resultado);
+
+    } else if (fs.lstatSync(caminho).isDirectory(caminho)) {
+        const arquivos = await fs.promises.readdir(caminho);
+        console.log(arquivos);
+
+    }
 }
 
 processarTexto(caminho);
